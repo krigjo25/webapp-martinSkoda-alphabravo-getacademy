6546a8413aafb793aa912934f56fa01cdbbbef4b
Kristoffer Gjøsund <73070534+krigjo25@users.noreply.github.com>
2024-08-26 14:30:24 +0200

    IC Creating projects

---
+++ virtual-pet/lib/js/food-o-meeter.js
@@ -0,0 +1,36 @@
+//  Controller
+//@krigjo25
+function foodOMeter()
+{
+    // Fetch the elements id.
+
+    //  Count seconds or 
+
+    //  Ensure the display is 100%¨
+        //  Print out 4 bars
+
+    //  Ensure the display is 75%¨
+        //  Print out 3 bars
+    
+    //  Ensure the display is 50%¨
+        //  Print out 2 bars
+    
+    //  Ensure the display is 25%¨
+        //  Print out 1 bars
+
+    return
+}
+
+function refill_food_o_meeter()
+{
+    //  Fetch Element id.
+
+    //  Ensure that food-o-meeter is not filled up
+        // ADD 25% to food-o-meter
+
+}
+
+function main()
+{
+
+}
