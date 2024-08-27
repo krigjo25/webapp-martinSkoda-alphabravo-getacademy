15ae17b97965dbd412783f451583301ee0fb55c1
Kristoffer Gjøsund <73070534+krigjo25@users.noreply.github.com>
2024-08-27 12:41:37 +0200

    Function create

--- virtual-pet/lib/js/food-o-meeter.js
+++ virtual-pet/lib/js/food-o-meeter.js
@@ -1,36 +1,70 @@
 //  Controller
 //@krigjo25
-function foodOMeter()
+// modal
+let html = "";
+let count_food = 0;
+
+//  Controller
+function foodOMeter(count_food)
 {
     // Fetch the elements id.
-
-    //  Count seconds or 
-
+    let id = document.getElementById('food-o-meeter');
+    console.log(id, count_food)
     //  Ensure the display is 100%¨
-        //  Print out 4 bars
 
+    if (count_food == 4)
+    {
+    //  Print out 4 bars
+    html = /*HTML*/`
+    <div class="segment"></div>
+    <div class="segment"></div>
+    <div class="segment"></div>
+    <div class="segment"></div>`;
+    }
     //  Ensure the display is 75%¨
-        //  Print out 3 bars
+    else if (count_food == 3)
+        {
+        //  Print out 4 bars
+        html = /*HTML*/`
+        <div class="segment"></div>
+        <div class="segment"></div>
+        <div class="segment"></div>`;
+        }
     
     //  Ensure the display is 50%¨
+    else if (count_food == 2)
+        {
         //  Print out 2 bars
-    
-    //  Ensure the display is 25%¨
-        //  Print out 1 bars
+        html = /*HTML*/`
+        <div class="segment"></div>
+        <div class="segment"></div>`;
+    }
 
-    return
+    //  Ensure the display is 25%
+    else if (count_food == 1)
+        {
+        //  Print out 1 bars
+        html = /*HTML*/`<div class="segment"></div>`;
+        
+        }
+    else if (count_food > 4 || count_food < 1){
+
+    }0
+    id.innerHTML = html; 
+    return;
 }
 
 function refill_food_o_meeter()
 {
-    //  Fetch Element id.
-
-    //  Ensure that food-o-meeter is not filled up
-        // ADD 25% to food-o-meter
+    return foodOMeter(food_count++);
 
 }
 
+
+// View
 function main()
 {
+    console.log('test main')
+    foodOMeter();
 
 }
