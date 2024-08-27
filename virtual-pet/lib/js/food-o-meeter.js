6f9f54061473b46558654e68c75a685b000c19a5
Kristoffer Gjøsund <73070534+krigjo25@users.noreply.github.com>
2024-08-27 13:31:56 +0200

    Modified virtual pet

--- virtual-pet/lib/js/food-o-meeter.js
+++ virtual-pet/lib/js/food-o-meeter.js
@@ -1,11 +1,23 @@
-//  Controller
+
 //@krigjo25
+
+function calculate(start, finish)
+{
+        // calculates only 
+        let sec = (ms) => ms / 1000;
+        if (start)
+            {return sec(Math.floor(finish - parseInt(start)));}
+}
+
 // modal
 let html = "";
-let count_food = 0;
+let count_food = 4;
+let min = 0;
+let sec = 0;
+let ms = 0;
 
 //  Controller
-function foodOMeter(count_food)
+function foodOMeter()
 {
     // Fetch the elements id.
     let id = document.getElementById('food-o-meeter');
@@ -16,6 +28,7 @@ function foodOMeter(count_food)
     {
     //  Print out 4 bars
     html = /*HTML*/`
+    Full:
     <div class="segment"></div>
     <div class="segment"></div>
     <div class="segment"></div>
@@ -26,6 +39,7 @@ function foodOMeter(count_food)
         {
         //  Print out 4 bars
         html = /*HTML*/`
+        Okay!:
         <div class="segment"></div>
         <div class="segment"></div>
         <div class="segment"></div>`;
@@ -36,6 +50,7 @@ function foodOMeter(count_food)
         {
         //  Print out 2 bars
         html = /*HTML*/`
+        Starting to become hungry !
         <div class="segment"></div>
         <div class="segment"></div>`;
     }
@@ -44,27 +59,71 @@ function foodOMeter(count_food)
     else if (count_food == 1)
         {
         //  Print out 1 bars
-        html = /*HTML*/`<div class="segment"></div>`;
+        html = /*HTML*/`
+        Starving !
+        <div class="segment"></div>`;
         
         }
-    else if (count_food > 4 || count_food < 1){
+    id.innerHTML = html;
+    main();
 
-    }0
-    id.innerHTML = html; 
     return;
 }
 
 function refill_food_o_meeter()
 {
-    return foodOMeter(food_count++);
+    if (count_food < 4)
+        {
+            count_food++;
+        }
+    return foodOMeter();
 
 }
 
+function decrease_bar()
+{
+    //  Fetch current time
+    //  Calculate the time
+    // return time;
+
+    if (sec == 3)
+    {
+        count_food--;
+    }
+    foodOMeter();
+    return;
+}
 
 // View
 function main()
 {
+    timer = setInterval(calculate_ms, 100);
     console.log('test main')
-    foodOMeter();
 
+}
+
+function calculate_ms()
+{
+
+    ms++;
+
+    if (ms == 100)
+    {
+        sec ++
+    }
+    else if (sec == 3)
+    {
+        return decrease_bar();
+    }
+    }
+    else if(min == 3)
+    {
+
+    }
+
+    if (sec == 3)
+    {
+        return decrease_bar();
+    }
+    return;
 }
