9cabd04bba06a41508694b11ab42bc33498ab6f8
Nicolayccxvi <nicolaykblondin@getacademy.no>
2024-09-11 09:45:43 +0200

    Update controller.js

--- martin-skoda/static/js/controller.js
+++ martin-skoda/static/js/controller.js
@@ -1,1 +1,31 @@
 // Functions
+// Controller: Kobler modellen og visningen
+const controller = {
+    handleEncounter: function() {
+        const encounter = model.getRandomEncounter();
+        
+        switch (encounter) {
+            case 0:
+                view.displayEncounter("Du ladet turbotrykket til maks, og slapp bremsen.");
+                break;
+            case 1:
+                view.displayEncounter("Du ladet turbotrykket til maks, og slapp bremsen.");
+                break;
+            case 2:
+                view.displayEncounter("Du ladet turbotrykket til maks, og slapp bremsen.");
+                break;
+            case 3:
+                view.displayEncounter("Du ladet turbotrykket til maks, og slapp bremsen.");
+                break;
+            case 4:
+                view.displayEncounter("Du ladet turbotrykket til maks, og slapp bremsen.");
+                break;
+            default:
+                view.displayEncounter("Noe uventet skjedde... mystisk.");
+                break;
+        }
+    }
+};
+
+// Starter encounter
+controller.handleEncounter();
