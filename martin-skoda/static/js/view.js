2eb754b915165ac9f6f80f4966a6e27993c26838
Kristoffer Gjøsund <73070534+krigjo25@users.noreply.github.com>
2024-09-11 10:39:12 +0200

    IC - Oppdatert Css, Lagt til et bilde av en rusten bil

--- martin-skoda/static/js/view.js
+++ martin-skoda/static/js/view.js
@@ -6,8 +6,7 @@ function mainView(){
             <div class="road-map"></div>
             <aside id="values" class="values">
                 <div id="bar-o-meeter-container">
-                    <div id="speed-label" class="speed-label">Speedster:</div>
-                    <div id="speed-o-meeter" class="speed-o-meeter"></div>
+                    
 
                     <div id="annoyed-label" class="annoyed-label">Grandma's annoyance:</div>
                     <div id="annoyed-o-meeter" class="annoyed-o-meeter"></div>
@@ -15,11 +14,14 @@ function mainView(){
                     <div id="rep-label" class="rep-label">Reputation:</div>
                     <div id="rep-o-meeter" class="rep-o-meeter"></div>
 
+                    <div id="speed-label" class="speed-label">Speedster:</div>
+                    <div id="speed-o-meeter" class="speed-o-meeter"></div>
                     <div id="cool-label" class="cool-label">Fabiolus:</div>
                     <div id="cool-o-meeter" class="cool-o-meeter"></div>
                 </div>
 
-            <div id="vehicle"></div>
+            <div id="vehicle">
+            <img src="static/img/rusty.jpg" alt="rusty.jpg"></div>
 
             <div id="buddy-list"></div>
             
