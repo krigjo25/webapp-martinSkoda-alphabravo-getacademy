2eb64626e63fa9441f528e7214ca6fca495ecae3
Nicolayccxvi <nicolaykblondin@getacademy.no>
2024-09-12 14:58:37 +0200

    ryddet opp i kode, fyttet ting til view

--- martin-skoda/static/js/controller.js
+++ martin-skoda/static/js/controller.js
@@ -2,81 +2,54 @@
 //@nico Controller: Kobler sammen modell og view, bestemmer responsen og beregner hastigheten
 const controller = {
   handleEncounter: function () {
-    const randomPersonIndex = model.getRandomEncounter(people.length); // Velger en tilfeldig person
-    const randomCarPartIndex = model.getRandomEncounter(carParts.length); // Velger en tilfeldig bil-del
+    const randomPersonIndex = model.getRandomEncounter(people.length); 
+    const randomCarPartIndex = model.getRandomEncounter(carParts.length); 
 
     const person = people[randomPersonIndex];
     const carPart = carParts[randomCarPartIndex];
 
-    // Starter meldingen med en introduksjon
     let message = `${person.name} ser på bilen og legger merke til ${carPart.part}. `;
 
-    // Oppdater coolnessFactor og annoyedFactor basert på den valgte delen
     if (!onCar.includes(carPart.part)) {
-      // Legg til delens coolnessFactor og annoyedFactor bare hvis delen ikke allerede er på bilen
       if (coolValue < 100) {
         coolValue += carPart.coolnesFactor;
         if (coolValue > 100) {
           coolValue = 100;
         }
       }
-      if(annoyedValue < 100) {
-      annoyedValue += carPart.annoyedFactor;
-      if(annoyedValue > 100){
-        annoyedValue = 100
+      if (annoyedValue < 100) {
+        annoyedValue += carPart.annoyedFactor;
+        if (annoyedValue > 100) {
+          annoyedValue = 100;
+        }
       }
-      }
-      // Legg til bildelen i onCar
       onCar.push(carPart.part);
     }
 
-    // Velg riktig prompt basert på personens preferanser
     if (person.likesCar) {
       if (carPart.coolnesFactor > carPart.annoyedFactor) {
-        const randomCoolPrompt =
-          coolPrompts[model.getRandomEncounter(coolPrompts.length)];
+        const randomCoolPrompt = coolPrompts[model.getRandomEncounter(coolPrompts.length)];
         message += `${randomCoolPrompt}`;
       } else {
-        const randomNotCoolPrompt =
-          notCoolPrompts[model.getRandomEncounter(notCoolPrompts.length)];
+        const randomNotCoolPrompt = notCoolPrompts[model.getRandomEncounter(notCoolPrompts.length)];
         message += `${randomNotCoolPrompt}`;
       }
     }
 
-    // Hvis personen er irritert (Bestemor i dette tilfellet)
     if (person.isAnnoyed) {
-      const randomAnnoyedPrompt =
-        isAnnoyedPrompts[model.getRandomEncounter(isAnnoyedPrompts.length)];
+      const randomAnnoyedPrompt = isAnnoyedPrompts[model.getRandomEncounter(isAnnoyedPrompts.length)];
       message += ` ${randomAnnoyedPrompt}`;
     }
 
-    // Beregn hastigheten basert på den totale coolness factor
-    speedValue = model.calculateSpeed(coolValue); // Oppdaterer speedValue
+    speedValue = model.calculateSpeed(coolValue); 
 
-    // Oppdater meldingen og hastigheten i HTML
     view.displayEncounter(message, speedValue);
 
-    // Oppdater listen over installerte bildeler i HTML
     view.updateInstalledParts(onCar);
 
-    // Oppdater coolValue og annoyedValue i HTML
     view.updateCoolValue(coolValue);
-    view.updateAnnoyedValue(annoyedValue); // Lag denne funksjonen i view.js
+    view.updateAnnoyedValue(annoyedValue); 
   },
 };
 
-// Sett en timer som trigges hvert 5. sekund for å utføre et nytt encounter
-setInterval(controller.handleEncounter, 5000); // Utfører handleEncounter hver 5. sekund
-//amy
-
-Rep();
-function Rep() {
-  const repmeter = document.getElementById("rep-o-meeter");
-
-  //kan brukes på for å øke hvor mye rep man får fra coolvlue:  respectValue += (coolValue / 4)
-
-  // forandrer baren etter % av Rep
-  repmeter.innerHTML = respectValue + "%";
-  repmeter.style.width = respectValue + "%";
-  console.log(respectValue);
-}
+setInterval(controller.handleEncounter, 5000);
--- martin-skoda/static/js/view.js
+++ martin-skoda/static/js/view.js
@@ -2,33 +2,67 @@
 mainView();
 function mainView() {
     document.getElementById("app").innerHTML = /*HTML*/ `
+        <div class="road-map"></div>
+        <aside id="values" class="values">
+            <div id="bar-o-meeter-container">
 
-            <div class="road-map"></div>
-            <aside id="values" class="values">
-                <div id="bar-o-meeter-container">
+                <div id="annoyed-label" class="annoyed-label">Grandma's annoyance:</div>
+                <div id="annoyed-o-meeter" class="annoyed-o-meeter" style="width: 0%">0%</div>
 
-                    <div id="annoyed-label" class="annoyed-label">Grandma's annoyance:</div>
-                    <div id="annoyed-o-meeter" class="annoyed-o-meeter" style="width: 0%">0%</div>
+                <div id="rep-label" class="rep-label" >Reputation:</div>
+                <div id="rep-o-meeter" class="rep-o-meeter"  style="width: 0%">0%</div>
 
-                    <div id="rep-label" class="rep-label" >Reputation:</div>
-                    <div id="rep-o-meeter" class="rep-o-meeter"  style="width: 0%">0%</div>
-
-                    <div id="speed-label" class="speed-label">Speedster:</div>
-                    <div id="speed-o-meeter" class="speed-o-meeter">0 km/t</div>
-                    <div id="cool-label" class="cool-label">Fabiolus:</div>
-                    <div id="cool-o-meeter" class="cool-o-meeter" style="width: 0%" >0%</div>
-                </div>
-
-            <div id="vehicle">
-            <img src="static/img/rusty.jpg" alt="rusty.jpg"></div>
-
-            <div id="buddy-list">
-                <div class="prompts">${}</div>
-                <button>button1</button>
-                <button>button2</button>
+                <div id="speed-label" class="speed-label">Speedster:</div>
+                <div id="speed-o-meeter" class="speed-o-meeter">0 km/t</div>
+                <div id="cool-label" class="cool-label">Fabiolus:</div>
+                <div id="cool-o-meeter" class="cool-o-meeter" style="width: 0%" >0%</div>
             </div>
-            <div id="aqured-parts"></div>
-        </aside>`;
+
+        <div id="vehicle">
+        <img src="static/img/rusty.jpg" alt="rusty.jpg"></div>
+
+        <div id="buddy-list">
+            <div class="prompts"></div>
+            <button>button1</button>
+            <button>button2</button>
+        </div>
+        <div id="aqured-parts"></div>
+    </aside>`;
 }
+//nico
+const view = {
+    displayEncounter: function (message, speed) {
+        console.log(`${message} Bilen kjører i ${speed} km/t.`);
 
-
+        const speedDisplay = document.getElementById("speed-o-meeter");
+        if (speedDisplay) {
+            speedDisplay.innerHTML = `${speed} km/t`;
+        }
+    },
+//amy
+    updateCoolValue: function (coolValue) {
+        const coolMeter = document.getElementById("cool-o-meeter");
+        coolMeter.style.width = coolValue + "%";
+        coolMeter.innerHTML = coolValue + "%";
+    },
+//yanis
+    updateAnnoyedValue: function (annoyedValue) {
+        const annoyedMeter = document.getElementById("annoyed-o-meeter");
+        annoyedMeter.style.width = annoyedValue + "%";
+        annoyedMeter.innerHTML = annoyedValue + "%";
+    },
+//nico
+    updateInstalledParts: function (onCar) {
+        const partsDisplay = document.getElementById("aqured-parts");
+        partsDisplay.innerHTML = `<ul>${onCar
+            .map((part) => `<li>${part}</li>`)
+            .join("")}</ul>`;
+    },
+//yanis
+    Rep: function () {
+        const repmeter = document.getElementById("rep-o-meeter");
+        repmeter.innerHTML = respectValue + "%";
+        repmeter.style.width = respectValue + "%";
+        console.log(respectValue);
+    }
+};
